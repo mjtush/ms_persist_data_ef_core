08c4516852669cd756eb8613800d96ae87cf063c
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-06-05 21:31:15 +0200

    Entities in Models updated

--- ContosoPizza/Models/Topping.cs
+++ ContosoPizza/Models/Topping.cs
@@ -1,7 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace ContosoPizza.Models;
 
 public class Topping
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+
+    public decimal Calories { get; set; }
+
+    // to omit circular reference
+    [JsonIgnore]
+    // to make Pizza-Topping a many-to-many relationship
+    public ICollection<Pizza>? Pizzas { get; set; }
 }
